7be5d550e651d14a9dcf41673975c7fd66c82413
HarshaVardhanBabu <amail2harsha@gmail.com>
2019-09-03 07:59:45 +0530

    create fake data frames to work with

---
+++ create_df.py
@@ -0,0 +1,10 @@
+import pandas as pd
+# Thanks to jakevdp.github.io
+def make_df(cols, ind):
+    """Quickly make a DataFrame"""
+    data = {c: [str(c) + str(i) for i in ind]
+            for c in cols}
+    return pd.DataFrame(data, ind)
+
+# example DataFrame
+make_df('ABC', range(3))
