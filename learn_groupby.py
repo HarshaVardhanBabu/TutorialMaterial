d078c52a7cd62e21692d2a1492421bfbc89f0dbc
HarshaVardhanBabu <amail2harsha@gmail.com>
2019-09-03 08:26:05 +0530

    simple example to look at groupby

---
+++ learn_groupby.py
@@ -0,0 +1,12 @@
+df = pd.DataFrame({'Animal': ['Falcon', 'Falcon','Parrot', 'Parrot'],'Max Speed': [380., 370., 24., 26.]})
+df
+df.groupby(['Animal']).mean()
+##
+df = pd.DataFrame([('bird', 'Falconiformes', 389.0),
+('bird', 'Psittaciformes', 24.0),
+('mammal', 'Carnivora', 80.2),
+('mammal', 'Primates', np.nan),
+('mammal', 'Carnivora', 58)],
+index=['falcon', 'parrot', 'lion', 'monkey', 'leopard'],
+columns=('class', 'order', 'max_speed'))
+grouped = df.groupby('class')
